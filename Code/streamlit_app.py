7433d014b882be9a51aa04ef3c288b763b1fb481
Eric Bayless <ebay788@gmail.com>
2021-02-28 20:11:15 -0600

    added demo and video

--- Code/streamlit_app.py
+++ Code/streamlit_app.py
@@ -8,43 +8,88 @@ import time
 
 # Bib bounding box color
 color = [252, 15, 192]
+# Initialize rank order list
+rank = []
 
-# App Title and Description
+# App Title and Mode
 st.title('Race Bib Number Detector')
-st.text('Proof of Concept')
+st.sidebar.header("Mode")
+mode = st.sidebar.radio(
+    'Select Mode',
+    options=['Demo', 'Image', 'Video']
+)
 
-# Get Image from User
-user_file = st.file_uploader(label='Image for analysis:',
-    type=['jpg', 'png'])
+if mode == 'Image':
+    # Get Image from User
+    user_file = st.file_uploader(label='Image for analysis:',
+        type=['jpg', 'png'])
 
-# Display image and convert for predicting
-if user_file != None:
+    button_loc = st.empty()
+    
+    # Display image and convert for predicting
+    if user_file != None:
+            text_loc = st.empty()
+            text_loc.text('This is your image:')
+            # Convert the file to an opencv image.
+            # From: https://github.com/streamlit/streamlit/issues/888
+            file_bytes = np.asarray(bytearray(user_file.read()), dtype=np.uint8)
+            img = cv.imdecode(file_bytes, 1)
+            img_loc = st.empty()
+            img_loc.image(img, channels='BGR')
+
+            if(button_loc.button('Detect')):
+
+                # get bib prediction
+                start = time.time() # start timing prediction
+                output = Detector.get_bibs(img)
+                end = time.time() # end timing prediction
+
+                for detection in output:
+                    # draw bouding box on original image
+                    (x, y, w, h) = detection[1]
+                    img = cv.rectangle(img,(x,y),(x+w,y+h),color,5)
+                    # add bib number to original image
+                    rbn = detection[0]
+                    cv.putText(img, str(rbn), (x, y - 25), cv.FONT_HERSHEY_SIMPLEX, 2, color, 4)
+
+                #display detection time and annotated image
+                text_loc.text(f'Detection time: {round(end - start, 2)} seconds')
+                img_loc.image(img, channels='BGR')
+else:
+    if mode == 'Demo':
+        video_file = open('../Data/bib_detector_demo_edit.mp4', 'rb')
+    elif mode == 'Video':
+        video_file = st.file_uploader(label='Video for analysis:',
+        type=['mp4'])
+
+    button_loc = st.empty()
     text_loc = st.empty()
-    text_loc.text('This is your image:')
-    # Convert the file to an opencv image.
-    # From: https://github.com/streamlit/streamlit/issues/888
-    file_bytes = np.asarray(bytearray(user_file.read()), dtype=np.uint8)
-    img = cv.imdecode(file_bytes, 1)
-    img_loc = st.empty()
-    img_loc.image(img, channels='BGR')
+    video_loc = st.empty()
 
-rbn_list = []
-button_loc = st.empty()
-if(button_loc.button('Detect')):
+    if video_file != None:
+        video_bytes = video_file.read()
+        video_loc.video(video_bytes)
 
-    # get bib prediction
-    start = time.time() # start timing prediction
-    output = Detector.get_bibs(img)
-    end = time.time() # end timing prediction
+        if(button_loc.button('Detect')):
+            cap = cv.VideoCapture('../Data/bib_detector_demo_edit.mp4')
+            cap.set(cv.CAP_PROP_FPS, 25)
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                # get bib prediction
+                start = time.time() # start timing prediction
+                output = Detector.get_bibs(frame, single=True)
+                end = time.time() # end timing prediction
 
-    for detection in output:
-        # draw bouding box on original image
-        (x, y, w, h) = detection[1]
-        img = cv.rectangle(img,(x,y),(x+w,y+h),color,5)
-        # add bib number to original image
-        rbn = detection[0]
-        cv.putText(img, str(rbn), (x, y - 25), cv.FONT_HERSHEY_SIMPLEX, 2, color, 4)
+                for detection in output:
+                    # draw bouding box on original image
+                    (x, y, w, h) = detection[1]
+                    img = cv.rectangle(frame,(x,y),(x+w,y+h),color,5)
+                    # add bib number to original image
+                    rbn = detection[0]
+                    cv.putText(frame, str(rbn), (x, y - 25), cv.FONT_HERSHEY_SIMPLEX, 2, color, 4)
 
-    #display detection time and annotated image
-    text_loc.text(f'Detection time: {round(end - start, 2)} seconds')
-    img_loc.image(img, channels='BGR')
+                #display detection time and annotated image
+                text_loc.text(f'Detection time: {round(end - start, 2)} seconds')
+                video_loc.image(frame, channels='BGR')
